671474b31026d922d9da912eba6aeaab62ad62e3
donnellyg <donnelly.gus@gmail.com>
2014-07-24 22:56:41 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Creature.h
+++ GameOfLife.spritebuilder/Creature.h
@@ -1,21 +1,13 @@
-//
-//  Creature.h
-//  GameOfLife
-//
-//  Created by Gus Donnelly on 7/24/14.
-//  Copyright (c) 2014 Apportable. All rights reserved.
-//
-
 #import "CCSprite.h"
 
 @interface Creature : CCSprite
 
-
+// stores the current state of the creature
 @property (nonatomic, assign) BOOL isAlive;
-@property (nonatomic, assign) int livingNeighbors;
 
-- (id)initCreature; //initializer
-- (void)setIsAlive:(BOOL)newState;
+// stores the amount of living neighbors
+@property (nonatomic, assign) NSInteger livingNeighbors;
 
+- (id)initCreature;
 
 @end
